55b3f3f77d8f0f83c193ecb934635803dacea662
Yuri Ferraz <yurigsf@dcc.ufrj.br>
2024-01-22 13:53:33 -0300

    Separa os scripts de instalação

--- installation.sh
+++ installation.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-sudo apt install -y git zsh resolvconf httpie helm curl ansible
+#sudo apt install -y git zsh resolvconf httpie helm curl ansible
+
+export DOT_FILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+scripts=(
+	kitty
+)
+
+for script in "${scripts[@]}"; do
+    . $DOT_FILES_DIR/scripts_install/$script.sh
+done
--- uninstallation.sh
+++ uninstallation.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo apt remove git zsh resolvconf httpie helm curl ansible fd-find flatpak git-gui libreoffice-calc libreoffice-writer mysql-server python3-pip unzip vim wget whiptail
+#sudo apt remove -y git zsh resolvconf httpie helm curl ansible fd-find flatpak git-gui libreoffice-calc libreoffice-writer mysql-server python3-pip unzip vim wget whiptail
+
+rm -rf ~/.local/kitty.app
+rm -rf ~/.local/share/applications/kitty*.desktop
+
